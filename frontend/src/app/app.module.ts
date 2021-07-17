f71461ddd6c24a9f4efab78d3f1fa38829d2949a
Всеволод Золотов <zolotov@isands.ru>
2021-07-18 00:13:49 +0400

    add theme switching

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginModule } from './modules/login/login.module';
 import { ChatModule } from './modules/chat/chat.module';
+
+import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 
 @NgModule({
@@ -16,6 +18,7 @@ import { MainLayoutComponent } from './components/main-layout/main-layout.compon
     BrowserAnimationsModule,
     LoginModule,
     ChatModule,
+    MatSlideToggleModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
