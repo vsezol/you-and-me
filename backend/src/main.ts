7574bf2ef48371b73d38572a6ee77fee92c3a46f
Всеволод Золотов <zolotov@isands.ru>
2021-08-01 16:53:23 +0300

    config env

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,8 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 3000;
+
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+
+  await app.listen(PORT, () => {
+    console.log(`[Nest] app started on port ${PORT}`);
+  });
 }
 bootstrap();
